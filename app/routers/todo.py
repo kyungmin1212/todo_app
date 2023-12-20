ec9dc53cba83e11a2e43f58607c65b929543a055
kyungmin1212 <uiuiui12@naver.com>
2023-12-21 02:47:16 +0900

    HTTPException을 사용하여 404 응답코드 반환

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, HTTPException, status
 from app.schemas.model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
@@ -25,7 +25,10 @@ async def get_single_todo(
         if todo.id == todo_id:
             return {"todo": todo}
 
-    return {"message": "Todo with supplied ID doesn`t exist."}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn`t exist",
+    )
 
 
 @todo_router.put("/todo/{todo_id}")
@@ -38,7 +41,10 @@ async def update_todo(
             todo.item = todo_data.item
             return {"message": "Todo updated successfully."}
 
-    return {"message": "Todo with supplied ID doesn`t exist."}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn`t exist",
+    )
 
 
 @todo_router.delete("/todo/{todo_id}")
@@ -49,7 +55,10 @@ async def delete_single_todo(todo_id: int) -> dict:
             todo_list.pop(index)
             return {"message": "TOdo deleted successfully."}
 
-    return {"message": "Todo with supplied ID doesn`t exist."}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn`t exist",
+    )
 
 
 @todo_router.delete("/todo")
