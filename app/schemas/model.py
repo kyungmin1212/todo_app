983382482f8fd257fe9d6c9bc63c4899ec5ac668
kyungmin1212 <uiuiui12@naver.com>
2023-12-21 02:03:32 +0900

    model_config 추가

--- app/schemas/model.py
+++ app/schemas/model.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    model_config = {
+        "json_schema_extra": {"examples": [{"id": 1, "item": "Example Schema!"}]}
+    }
