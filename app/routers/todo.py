2b6999eb6004abf63b8ad20716f35ea0c78749b4
kyungmin1212 <uiuiui12@naver.com>
2023-12-21 01:44:10 +0900

    경로 매개변수 함수 작성

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -15,3 +15,12 @@ async def add_todo(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {"todo": todo}
+
+    return {"message": "Todo with supplied ID doesn`t exist."}
